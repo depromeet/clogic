0e68075ceda240024d814f72cc5eae81080b26a4
sichoi <clogic@toss.im>
2020-07-22 02:13:44 +0900

    예외케이스 에러 메시지 노출

--- src/packages/index.ts
+++ src/packages/index.ts
@@ -3,7 +3,11 @@ import { RenderFunction, HTMLElementTagNameList } from "./types";
 export const createElement = (rootFunction: RenderFunction, id: string) => {
   const element = rootFunction();
   const rootElement = document.getElementById(id);
-  rootElement?.appendChild(element);
+
+  if (!rootElement) {
+    throw Error("root element의 id 지정이 잘못되었습니다.");
+  }
+  rootElement.appendChild(element);
 };
 
 export const render = (
